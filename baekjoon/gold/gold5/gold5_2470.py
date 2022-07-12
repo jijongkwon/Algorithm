f9d87873f2e2c7e5b2e6f30c504e5c6ae24f8d95
jijongkwon <help3451@naver.com>
2022-07-12 21:05:53 +0900

    update

--- baekjoon/gold/gold5/gold5_2470.py
+++ baekjoon/gold/gold5/gold5_2470.py
@@ -1,17 +1,19 @@
 import sys
 
+
 def imP():
     return sys.stdin.readline()
 
+
 N = int(imP())
-Solution = list(map(int, imP().split())) # 용액
+Solution = list(map(int, imP().split()))  # 용액
 if N == len(Solution):
     Solution.sort()
-    start = 0 #시작 인덱스
-    end = N-1 # 끝 인데스
-    min = 10000000000000 # 0에 가까운 값
+    start = 0  # 시작 인덱스
+    end = N-1  # 끝 인데스
+    min = 10000000000000  # 0에 가까운 값
 
-    answer = [] #정답
+    answer = []  # 정답
 
     while start < end:
         s_L = Solution[start]
@@ -19,13 +21,13 @@ if N == len(Solution):
 
         sum = s_L + s_R
 
-        if abs(sum) < min : # 절댓값으로 비교
+        if abs(sum) < min:  # 절댓값으로 비교
             min = abs(sum)
             answer = [s_L, s_R]
-        
-        if sum < 0 :
+
+        if sum < 0:
             start += 1
-        else :
+        else:
             end -= 1
 
     print(answer[0], answer[1])
