10ff063f65ccfc7bb149e1a7e8bcbc9f10b0a000
jijongkwon <help3451@naver.com>
2023-05-15 16:53:43 +0900

    feat : level2 금고털이 구현

---
+++ src/main/java/softeer/level2/Solution.java
@@ -0,0 +1,39 @@
+package softeer.level2;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int W = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        ArrayList<int[]> list = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(in.readLine());
+            list.add(new int[]{Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())});
+        }
+
+        Collections.sort(list,(o1,o2)->o2[1]-o1[1]);
+
+        int answer = 0;
+
+        for(int[] item:list){
+            if(W<=item[0]){
+                answer += W*item[1];
+                break;
+            }else{
+                answer += item[0]*item[1];
+                W-=item[0];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
