72b40c34cf90efbe973de9b9928885b811f0f1e9
jijongkwon <help3451@naver.com>
2024-02-05 14:32:17 +0900

    [Gold V] Title: 탑, Time: 848 ms, Memory: 101572 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,7 +5,15 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 /**
- * gold5_2493
+ * @author 지종권
+ * @date 2024. 2. 5.
+ * @link https://www.acmicpc.net/problem/2493
+ * @keyword_solution 
+ * stack (앞, 뒤 비교)
+ * @input 
+ * @output   
+ * @time_complex nlogn 이하
+ * @perf 
  */
 public class Main {
 
@@ -15,20 +23,23 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         
+        StringBuilder sb = new StringBuilder();
         Stack<int[]> stack = new Stack<>();
         for(int i = 1; i <= n; i++) {
             int top = Integer.parseInt(st.nextToken());
             while(!stack.isEmpty()) {
                 if(stack.peek()[1] >= top) {
-                    System.out.print(stack.peek()[0] + " ");
+                    sb.append(stack.peek()[0] + " ");
                     break;
                 }
                 stack.pop();
             }
             if(stack.isEmpty()) {
-                System.out.print("0 ");
+                sb.append(0 + " ");
             }
             stack.push(new int[] {i, top}); 
         }
+        
+        System.out.println(sb.toString());
     }
 }
