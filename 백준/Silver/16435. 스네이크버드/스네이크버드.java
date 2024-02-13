7a03330014fa239b9ea8fdca983fe528d48cec00
jijongkwon <help3451@naver.com>
2024-02-13 10:40:07 +0900

    [Silver V] Title: 스네이크버드, Time: 284 ms, Memory: 21204 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author 지종권
+ * @date 2024. 2. 13.
+ * @link https://www.acmicpc.net/problem/16435
+ * @keyword_solution  
+ * 1. 자신의 길이 보다 작거나 or 같은 높이에 있는 과일을 먹을 수 있음
+ * 2. 최대길이 
+ * @input 
+ * @output   
+ * @time_complex  
+ * @perf 
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int l = sc.nextInt();
+		
+		int[] fs = new int[n];
+		for(int i = 0; i < n; i++) {
+			int f = sc.nextInt();
+			
+			fs[i] = f;
+		}
+		Arrays.sort(fs);
+		
+		for(int i = 0; i < n; i++) {
+			if(fs[i] <= l) {
+				l++;
+			}
+		}
+		
+		System.out.println(l);
+	}
+}
