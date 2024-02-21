eeff40494487da5d4359d5c39cd06477cee59d0d
jijongkwon <help3451@naver.com>
2024-02-21 15:32:27 +0900

    [Gold IV] Title: 감시, Time: 156 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -16,176 +16,106 @@ import java.io.*;
  * @perf 
  */
 public class Main {
-	public static int N, M;
-	public static int[][] map;
-	public static int[][] copyMap;
-	public static int[] output;
-	public static ArrayList<CCTV> cctvList;
-	public static int[] dx = {-1, 0, 1, 0};
-	public static int[] dy = {0, 1, 0, -1};
-	public static int answer = Integer.MAX_VALUE;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+	static int N;
+	static int[][] map;
+	static int[][] dirs = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } }; // 상, 좌, 우, 하
+	static int[][][] lookIdx = { { { 0 }, { 1 }, { 2 }, { 3 } }, // 상, 좌, 우, 하
+							   { { 0, 3 }, { 1, 2 } }, // 상하, 좌우
+							   { { 0, 2 }, { 2, 3 }, { 3, 1 }, { 1, 0 } }, // 상우, 우하, 하좌, 좌상
+							   { { 0, 1, 2 }, { 0, 2, 3 }, { 1, 2, 3 }, { 0, 1, 3 } }, // 상좌우, 상우하, 좌하우, 하좌상
+							   { { 0, 1, 2, 3 } } }; // 상좌우하
+	static int M;
+	static ArrayList<int[]> cctv;
+	static int cctvSize;
+	static int result = Integer.MAX_VALUE;
+	static int zeroCnt = 0;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		cctvList = new ArrayList<>();
-
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				map[i][j] = sc.nextInt();
-
-				if(map[i][j] != 0 &&  map[i][j] != 6) {
-					cctvList.add(new CCTV(map[i][j], i, j));
+		cctv = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				
+				// cctv 리스트 넣기
+				if (map[i][j] > 0 && map[i][j] < 6) {
+					cctv.add(new int[] { i, j });
+				}
+				
+				// 사각지대 개수 세기
+				else if(map[i][j]==0) {
+					zeroCnt++;
 				}
 			}
 		}
+		
+		// 시시티비 크기
+		cctvSize = cctv.size();
+		
+		// 감시 시작
+		dfs(0,0);
 
-		// 순열
-		output = new int[cctvList.size()];
-		permutation(0, cctvList.size());
-
-		System.out.println(answer);
+		// 출력
+		System.out.println(result);
 	}
 
-	// 순열
-	public static void permutation(int depth, int r) {
+	private static void dfs(int idx,int cnt) {
 		// base
-		if(depth == r) {
-			copyMap = copyMap();
-						
-			// cctv번호와 순열로 뽑혀진 방향에 맞는 상하좌우 방향 설정 
-			for(int i = 0; i < cctvList.size(); i++) {
-				direction(cctvList.get(i), output[i]);
-			}
-			
-			// 사각 지대 구하기 
-			getBlindSpot();
-
+		if (idx == cctvSize) {
+			result = Math.min(result, zeroCnt-cnt);
 			return;
 		}
 		
 		// inductive
-		for(int i = 0; i < 4; i++) {
-			output[depth] = i;
-			permutation(depth+1, r);
+		int end = 4;
+		int[] cv = cctv.get(idx);
+		int cctvNum = map[cv[0]][cv[1]];
+
+		// 4 2 4 4 1 cctv에 따른 방향 개수
+		if (cctvNum == 5) {
+			end = 1;
+		} else if (cctvNum == 2)
+			end = 2;
+		for (int i = 0; i < end; i++) {
+			// 맵에 감시하는 곳 채우기
+			dfs(idx + 1, cnt+see(cctvNum, i, -1, cv));
+			// 초기화
+			see(cctvNum, i, 1, cv);
 		}
+
 	}
 
-	// 각 cctv 번호와 순열로 뽑혀진 방향에 맞게 감시 
-	public static void direction(CCTV cctv, int d) {
-		int cctvNum = cctv.num;
-
-		if(cctvNum == 1) {
-			if(d == 0) watch(cctv, 0); // 상 
-			else if(d == 1) watch(cctv, 1); // 우 
-			else if(d == 2) watch(cctv, 2); // 하  
-			else if(d == 3) watch(cctv, 3); // 좌 
-		} else if(cctvNum == 2) {
-			if(d == 0 || d == 2) {
-				watch(cctv, 0); watch(cctv, 2); // 상하 
-			} else {
-				watch(cctv, 1); watch(cctv, 3); // 좌우 
-			}
-		} else if(cctvNum == 3) {
-			if(d == 0) {
-				watch(cctv, 0); // 상우 
-				watch(cctv, 1);
-			} else if(d == 1) { 
-				watch(cctv, 1); // 우하 
-				watch(cctv, 2);
-			} else if(d == 2) { 
-				watch(cctv, 2); // 하좌 
-				watch(cctv, 3);
-			} else if(d == 3) { 
-				watch(cctv, 0); // 좌상 
-				watch(cctv, 3);
-			}
-		} else if(cctvNum == 4) {
-			if(d == 0) {
-				watch(cctv, 0); // 좌상우 
-				watch(cctv, 1);
-				watch(cctv, 3);
-			} else if(d == 1) {
-				watch(cctv, 0); // 상우하 
-				watch(cctv, 1);
-				watch(cctv, 2);
-			} else if(d == 2) {
-				watch(cctv, 1); // 좌하우 
-				watch(cctv, 2);
-				watch(cctv, 3);
-			} else if(d == 3) {
-				watch(cctv, 0); // 상좌하 
-				watch(cctv, 2);
-				watch(cctv, 3);
-			}
-		} else if(cctvNum == 5) { // 상우하좌
-			watch(cctv, 0);
-			watch(cctv, 1);
-			watch(cctv, 2);
-			watch(cctv, 3);
-		}
-	}
-	
-	// BFS로 방향에 맞게 감시 
-	public static void watch(CCTV cctv, int d) {
-		Queue<CCTV> queue = new LinkedList<>();
-		boolean[][] visited = new boolean[N][M];
-
-		queue.add(cctv);
-		visited[cctv.x][cctv.y] = true;
-
-		while(!queue.isEmpty()) {
-			int nx = queue.peek().x + dx[d];
-			int ny = queue.poll().y + dy[d];
-
-			// 범위를 벗어나거나 벽을 만나면 끝 
-			if(nx < 0 || nx >= N || ny < 0 || ny >= M || copyMap[nx][ny] == 6) { 
-				break;
-			}
-
-			// 감시
-			if(copyMap[nx][ny] == 0) { 
-				copyMap[nx][ny] = -1; 
-				queue.add(new CCTV(cctv.num, nx, ny));
-			} else {  
-				queue.add(new CCTV(cctv.num, nx, ny)); 
-			}
-		}
-	}
-	
-	// 사각 지대 구하기 
-	public static void getBlindSpot() {
+	private static int see(int cctvNum, int turnIdx, int flag, int[] loc) {
+		int[] turn = lookIdx[cctvNum - 1][turnIdx]; 
 		int cnt = 0;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				if(copyMap[i][j] == 0) {
+		
+		for (int d = 0; d < turn.length; d++) { //4방향
+			int r = loc[0];
+			int c = loc[1];
+			while (true) {//한방향으로 쭉
+				r += dirs[turn[d]][0];
+				c += dirs[turn[d]][1];
+				if (r < 0 || r > N - 1 || c < 0 || c > M - 1 || map[r][c] == 6)
+					break;
+				if (map[r][c] > 0 && map[r][c] < 6)
+					continue;
+				if (map[r][c] == 0)
 					cnt++;
-				}
+
+				map[r][c] += flag;
 			}
-		}
-		answer = Math.min(answer, cnt);
-	}
-	
-	static int[][] copyMap(){
-		copyMap = new int[N][M];
-		for(int i = 0; i < map.length; i++) {
-			System.arraycopy(map[i], 0, copyMap[i], 0, map[i].length);
+
 		}
 		
-		return copyMap;
+		return cnt;
 	}
 
-	static class CCTV {
-		int num;
-		int x;
-		int y;
-
-		CCTV(int num, int x, int y) {
-			this.num = num;
-			this.x = x;
-			this.y = y;
-		}
-	}
 }
