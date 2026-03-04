086c9da76ce7dfd9bac8a61b26f75138774da8df
jijongkwon <help3451@naver.com>
2026-03-04 21:35:33 +0900

    [level 3] Title: 부대복귀, Time: 138.68 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,48 +1,61 @@
 import java.util.*;
 
+/*
+    100,000
+    500,000
+    500
+    
+    500 bfs -> 말안됨
+    -> 다익스트라
+    다만 가중치 1이기때문에, dist[] 만 기록하면 됨 (클래스 생성x)
+    
+*/
 class Solution {
-    int[] cost;
+    int [] dist;
     List<Integer>[] graph;
     
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        graph = new List[n + 1];
-        for(int i = 0 ; i <= n; ++i)
-            graph[i] = new ArrayList();
+        dist = new int[n + 1];
+        Arrays.fill(dist, -1);
+        graph = new ArrayList[n + 1];
         
-        for(int[] data : roads){
-            // 양방향 연결
-            graph[data[0]].add(data[1]);
-            graph[data[1]].add(data[0]);
+        for(int i = 0; i <= n; i++){
+            graph[i] = new ArrayList<>();            
         }
         
-        cost = new int[n + 1];
-        Arrays.fill(cost, -1);
-        search(destination);
-        
-        int[] answer = new int[sources.length];
-        for(int i = 0; i < sources.length; i++){
-            answer[i] = cost[sources[i]];
+        for(int i = 0; i < roads.length; i++){
+            int start = roads[i][0];
+            int end = roads[i][1];
+            
+            graph[start].add(end);
+            graph[end].add(start);
         }
         
-        return answer;
-    }
-    
-    public void search(int start){
+        // 기록
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        cost[start] = 0;
+        queue.add(destination);
+        dist[destination] = 0;
         
         while(!queue.isEmpty()){
             int cur = queue.poll();
-            int len = graph[cur].size();
             
-            for(int i = 0 ; i < len; i++){
-                int next = graph[cur].get(i);
-                if(cost[next] == -1){
-                    cost[next] = cost[cur] + 1;
-                    queue.add(next);
+            for(int next : graph[cur]){
+                if(dist[next] != -1){
+                    continue;                    
                 }
+                
+                dist[next] = dist[cur] + 1;
+                queue.add(next);
             }
         }
+        
+        int[] answer = new int[sources.length];
+        for(int i = 0; i < sources.length; i++){
+            int node = sources[i];
+            
+            answer[i] = dist[node];
+        }
+        
+        return answer;
     }
 }
